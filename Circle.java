0f7a322cc348f9a2b7d6946b1585d8bfb0a80238
Ramlakhan patel <138600057+ram19966p@users.noreply.github.com>
2025-02-19 23:03:05 +0530

    "DSA Series"

---
+++ Circle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Circle {
+    public static void main(String[] arg){
+        Scanner obj  = new Scanner(System.in);
+        double a ;
+        int  r ;
+        System.out.println("Enter the value of radius"); 
+        r = obj.nextInt();
+           a = 2 * 3.14 * r ;
+
+           System.out.println(" The Area of circle  is "+a);
+    }
+    
+}
